0e1b8cb8e2f66dd47d2fd376429402d3fb8d1f20
afdesk <work@afdesk.com>
2023-05-14 14:06:48 +0600

    update calculation of count items

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # app
 
-You can use this image for any models.
+Docker образ для запуска модели в виде web-сервиса.
 
 You should mount a volume with the model, and have to set up the shape as an environment variable `MODEL_SHAPE`:
 ```sh
 $ docker build -t backend:latest .
 
-$ docker run -it --rm -e MODEL_SHAPE="3,46,9" -v /home/amf/ras-internship/model.h5:/app/model.h5 -p 8080:8080 backend:latest
+$ docker run -it --rm -e MODEL_SHAPE="3,4" -v /home/amf/ras-internship/model.h5:/app/model.h5 -p 8080:8080 backend:latest
 ```
+**note**: `MODEL_SHAPE` не содержит общее количество тензоров, т.е. пропускает первый индекс. Для набора данныз (90, 3, 4) `MODEL_SHAPE="3,4"`
--- backend/app/app.py
+++ backend/app/app.py
@@ -1,27 +1,39 @@
 from fastapi import FastAPI
+
+import logging
 from pydantic import BaseModel
 import tensorflow as tf
+
+from functools import reduce
 import numpy as np
 import os
 
 # Define the FastAPI app
 app = FastAPI()
 
-# Define the input data schema
-class InputData(BaseModel):
-    input: list
+# Create a logger object
+logger = logging.getLogger(__name__)
 
 # Load the pre-trained h5 model
 model = tf.keras.models.load_model("model.h5")
+
+# Define the input data schema
+class InputData(BaseModel):
+    input: list
 
 # Define the prediction endpoint
 @app.post("/predict")
 def predict(data: InputData):
     # Convert the input data to a numpy array
-    env_shape = os.environ['MODEL_SHAPE']
-    shape = tuple( int(s.strip()) for s in env_shape.split(',') )
-    input_data = np.array(data.input).reshape(shape)
-    # Make a prediction using the loaded model
+    env_shape = os.environ.get('MODEL_SHAPE', '')
+    if env_shape == '':
+        env_shape = '3,4'
+
+    pre_shape = list( int(s.strip()) for s in env_shape.split(',') )
+    logger.warn(pre_shape)
+    y = len(data.input) // reduce(lambda x, y: x * y, pre_shape)
+
+    input_data = np.array(data.input).reshape((y, *pre_shape))
     prediction = model.predict(input_data)
 
     # Return the prediction
